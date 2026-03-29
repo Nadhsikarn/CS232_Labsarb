a0556f688fc1006b675d8f0fd9ece58409466dfd
Nadhsikarn <Nadhsikarn.jon@dome.tu.ac.th>
2026-03-29 22:54:49 +0700

    update data mapping in lambda based on api-contract.json

--- labsarb-line-handler/lambda-receive-request.py
+++ labsarb-line-handler/lambda-receive-request.py
@@ -2,9 +2,29 @@
 import json
 
 def lambda_handler(event, context):
+    body = json.loads(event.get('body', '{}'))
 
-    print("EVENT =", event)
+    # ดึง event แรกที่ส่งมาจาก LINE
+    line_event = body.get('events', [{}])[0]
+    
+    # ดึงค่า userId มาใช้เป็น student_id
+    student_id = line_event.get('source', {}).get('userId', 'unknown_id')
+    
+    # ดึง messageId ไว้ใช้ใน Task ถัดไป
+    message_id = line_event.get('message', {}).get('id', 'no_message_id')
+
+    upload_image_payload = {
+        "student_id": student_id,
+        "lab_id": "LAB01",  #mock
+        "image_url": f"pending_upload_for_{message_id}"
+    }
+
+    print("Mapped Data to Contract:", json.dumps(upload_image_payload))
+
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "ok"})
+        "body": json.dumps({
+            "status": "success",
+            "mapped_data": upload_image_payload
+        })
     }
